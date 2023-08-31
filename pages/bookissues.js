47608a39c68d729c62e070e5937db7c33e45ea5a
uday sankar das <104709080+Uday2012047@users.noreply.github.com>
2023-08-31 18:03:57 +0530

    Update and rename prescription.js to bookissues.js

--- pages/bookissues.js
+++ pages/bookissues.js
@@ -39,7 +39,7 @@ export default function prescription() {
     setProcessing(true);
 
     const ph = await axios.get(
-      `http://localhost:3000/api/getpatientnumber/?username=${username}`
+      `http://localhost:3000/api/getstudentnumber/?username=${username}`
     );
     const phone = ph.data.phone;
 
@@ -70,8 +70,8 @@ export default function prescription() {
         body: JSON.stringify({
           chatId: chatid,
           urlFile: imageUrl,
-          fileName: "prescription.jpg",
-          caption: `Your Prescriptions from Librarian${session.user.name}`,
+          fileName: "bookissues.jpg",
+          caption: `Your issued Book from Librarian${session.user.name}`,
         }),
       }
     );
@@ -147,7 +147,7 @@ export default function prescription() {
             <label
               for="formFileMultiple"
               class="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
-            >Choice Patient Prescription </label
+            >Choice Student Books </label
             >
             <input
               class="relative m-0 block w-full min-w-0 flex-auto rounded border-2 border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-300 focus:shadow-te-primary focus:outline-none hover:border-red-600 dark:border-neutral-400 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
